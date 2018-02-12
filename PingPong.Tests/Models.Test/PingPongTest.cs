caa933bbc5035a36dfb94fd415a5e7bc6f48d513
Hamza Naeem and Ian Goodrich <ian@>
2018-02-12 11:51:17 -0800

    ran tests, have tested core method for PingPong app.

--- PingPong.Tests/Models.Test/PingPongTest.cs
+++ PingPong.Tests/Models.Test/PingPongTest.cs
@@ -1,11 +1,66 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PingPong.Models;
+using PingPongs.Models;
 using System;
 
-namespace PingPong.Tests
+namespace PingPongs.Tests
 {
-  public class PingPong
+  [TestClass]
+  public class PingPongTest
   {
 
+    // [TestMethod]
+    // public void IsDivThree_DivisableBy_False()
+    // {
+    //   int Testint = 2;
+    //   PingPong testPong = new PingPong();
+    //   Assert.AreEqual(false, testPong.IsDiv(Testint));
+    // }
+    //
+    // [TestMethod]
+    // public void IsDivThree_DivisableBy3_True()
+    // {
+    //   int Testint = 9;
+    //   PingPong testPong = new PingPong();
+    //   Assert.AreEqual(true, testPong.IsDiv(Testint));
+    // }
+    //
+    // [TestMethod]
+    // public void IsDivThree_DivisableBy5_False()
+    // {
+    //   int Testint = 4;
+    //   PingPong testPong = new PingPong();
+    //   Assert.AreEqual(false, testPong.IsDiv(Testint));
+    // }
+    //
+    // [TestMethod]
+    // public void IsDivThree_DivisableBy5_True()
+    // {
+    //   int Testint = 10;
+    //   PingPong testPong = new PingPong();
+    //   Assert.AreEqual(true, testPong.IsDiv(Testint));
+    // }
+    [TestMethod]
+    public void IsDivThree_DivisableBy3And5_Pong()
+    {
+      int Testint = 5;
+      PingPong testPong = new PingPong();
+      Assert.AreEqual("Pong", testPong.IsDiv(Testint));
+    }
+
+    [TestMethod]
+    public void IsDivThree_DivisableBy3And5_PingPong()
+    {
+      int Testint = 15;
+      PingPong testPong = new PingPong();
+      Assert.AreEqual("PingPong", testPong.IsDiv(Testint));
+    }
+    [TestMethod]
+    public void IsDivThree_DivisableBy3And5_TestInt()
+    {
+      int Testint = 14;
+      PingPong testPong = new PingPong();
+      string TestIntToString = Testint.ToString();
+      Assert.AreEqual(TestIntToString, testPong.IsDiv(Testint));
+    }
   }
 }
